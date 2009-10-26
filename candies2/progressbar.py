6d0c91263c2fa144c0fa5118493848aaad0cf47b
dboucard <>
2009-10-26 14:30:31 +0000

    Enhancing progressbar (keep aspect ratio, test animation, etc.) ;

--- candies2/progressbar.py
+++ candies2/progressbar.py
@@ -9,7 +9,7 @@ class ProgressBar(clutter.Actor, clutter.Container):
     __gtype_name__ = 'ProgressBar'
     __gproperties__ = {
             'progression': (gobject.TYPE_FLOAT, 'Progression', 'Progression value',
-                0, 1, 0, gobject.PARAM_READWRITE),
+                0.0, 1.0, 0.0, gobject.PARAM_READWRITE),
     }
     def __init__(self, skin_path):
         clutter.Actor.__init__(self)
@@ -21,10 +21,29 @@ class ProgressBar(clutter.Actor, clutter.Container):
             clutter.Texture(os.path.join(skin_path, 'progress_middle.png')),
             clutter.Texture(os.path.join(skin_path, 'progress_right.png')),
         )
+        self.natural_height = 0
         for texture in self._textures:
             texture.show()
             texture.set_parent(self)
-        self.progression = 0.0
+            self.natural_height = \
+                      max(self.natural_height, texture.get_preferred_size()[3])
+        self._progression = 0.0
+    
+    def set_progression(self, value):
+        self._progression = value
+        self.notify("progression")
+        self.queue_relayout()
+
+    def get_progression(self):
+        return self._progression
+
+    def do_get_property(self, pspec):
+        if pspec.name == 'progression':
+            return self.get_progression()
+    
+    def do_set_property(self, pspec, value):
+        if pspec.name == 'progression':
+            self.set_progression(value)
     
     def do_get_preferred_width(self, for_height):
         minimal_width = \
@@ -35,23 +54,28 @@ class ProgressBar(clutter.Actor, clutter.Container):
             self._textures[self._TRACK_LEFT].get_preferred_width(for_height)[1] +
             self._textures[self._TRACK_RIGHT].get_preferred_width(for_height)[1]
         )
-        print minimal_width, natural_width
+        #print minimal_width, natural_width
         return minimal_width, natural_width
+    
+    def do_get_preferred_height(self, for_width):
+        return self.natural_height, self.natural_height
     
     def do_allocate(self, box, flags):
         box_width, box_height = self.get_preferred_size()[2:]
-        print box_width, 'x', box_height
+        #print box_width, 'x', box_height
         
         minimal_width = self.do_get_preferred_width(box_height)[0]
-        print 'minimal', minimal_width
+        #print 'minimal', minimal_width
         
         texture = self._textures[self._TRACK_LEFT]
         box_tl = clutter.ActorBox()
         box_tl.x1 = 0
         box_tl.y1 = 0
         if box_width > minimal_width:
+            texture.props.keep_aspect_ratio = True
             box_tl.x2 = texture.get_preferred_width(box_height)[1]
         else:
+            texture.props.keep_aspect_ratio = False
             box_tl.x2 = box_width / 2 - 1
         box_tl.y2 = box_height
         texture.allocate(box_tl, flags)
@@ -59,8 +83,10 @@ class ProgressBar(clutter.Actor, clutter.Container):
         texture = self._textures[self._TRACK_RIGHT]
         box_tr = clutter.ActorBox()
         if box_width > minimal_width:
+            texture.props.keep_aspect_ratio = True
             box_tr.x1 = box_width - texture.get_preferred_width(box_height)[1]
         else:
+            texture.props.keep_aspect_ratio = False
             box_tr.x1 = box_width / 2 + 1
         box_tr.y1 = 0
         box_tr.x2 = box_width
@@ -77,23 +103,23 @@ class ProgressBar(clutter.Actor, clutter.Container):
         
         texture = self._textures[self._PROGRESS_LEFT]
         texture.allocate(box_tl, flags)
-        if self.progression == 0.0:
+        if self._progression == 0.0:
             texture.hide()
         else:
             texture.show()
         
         texture = self._textures[self._PROGRESS_RIGHT]
         texture.allocate(box_tr, flags)
-        if self.progression == 1.0:
+        if self._progression == 1.0:
             texture.show()
         else:
             texture.hide()
         
         texture = self._textures[self._PROGRESS_MIDDLE]
         box_pm = clutter.ActorBox()
-        box_pm.x1 = box_tl.x2 + 1
+        box_pm.x1 = box_tl.x2
         box_pm.y1 = 0
-        box_pm.x2 = box_pm.x1 + (box_tr.x1 - box_tl.x2) * self.progression
+        box_pm.x2 = box_pm.x1 + (box_tr.x1 - box_tl.x2) * self._progression
         box_pm.y2 = box_height
         texture.allocate(box_pm, flags)
         
--- tests/test_progressbar.py
+++ tests/test_progressbar.py
@@ -3,6 +3,7 @@
 
 import clutter
 import os
+import gobject
 from candies2.progressbar import ProgressBar
 
 __path__ = os.path.dirname(os.path.abspath(__file__))
@@ -10,10 +11,64 @@ __path__ = os.path.dirname(os.path.abspath(__file__))
 stage = clutter.Stage()
 stage.connect('destroy', clutter.main_quit)
 
+# Animated progress bar
+label = clutter.Text()
+label.set_position(5, 5)
+label.set_text('Click on the progress bar bellow...')
+stage.add(label)
+
 bar = ProgressBar(os.path.join(__path__, 'progress_skin'))
-bar.set_size(600, 120)
+bar.props.progression = 0.0
+bar.set_position(5, 20)
+bar.set_size(630, 50)
+def update_label(bar, event, label):
+    label.set_text('%d %%' %(bar.props.progression * 100))
+def progress(bar):
+    bar.props.progression = min(round(bar.props.progression + 0.002, 3), 1.0)
+    return bar.props.progression < 1.0
+def on_button_press(bar, event):
+    bar.props.progression = 0.0
+    gobject.timeout_add(10, progress, bar)
+bar.set_reactive(True)
+bar.connect('button-press-event', on_button_press)
+bar.connect('notify::progression', update_label, label)
 stage.add(bar)
-bar.show()
+
+# Static 0%
+label = clutter.Text()
+label.set_position(5, 100)
+label.set_text('0 %')
+stage.add(label)
+
+bar = ProgressBar(os.path.join(__path__, 'progress_skin'))
+bar.props.progression = 0.0
+bar.set_position(5, 115)
+bar.set_size(630, 50)
+stage.add(bar)
+
+# Static 67%
+label = clutter.Text()
+label.set_position(5, 200)
+label.set_text('67 %')
+stage.add(label)
+
+bar = ProgressBar(os.path.join(__path__, 'progress_skin'))
+bar.props.progression = 0.667
+bar.set_position(5, 215)
+bar.set_size(630, 50)
+stage.add(bar)
+
+# Static 100%
+label = clutter.Text()
+label.set_position(5, 300)
+label.set_text('100 %')
+stage.add(label)
+
+bar = ProgressBar(os.path.join(__path__, 'progress_skin'))
+bar.props.progression = 1.0
+bar.set_position(5, 315)
+bar.set_size(630, 50)
+stage.add(bar)
 
 stage.show()
 clutter.main()
