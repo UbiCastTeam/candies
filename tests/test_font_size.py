20416ee5a1ea7273e36db554b71df9992325c3c2
Damien Boucard <damien.boucard@ubicast.eu>
2009-12-03 10:46:47 +0100

    Adding a script to see the rapport between fontsize and clutter.Text actor size ;

---
+++ tests/test_font_size.py
@@ -0,0 +1,19 @@
+import clutter
+
+lorem_ipsum = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed'\
+              ' do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+label = clutter.Text()
+label.set_text(lorem_ipsum)
+print '----------------------------     --------------------------'
+print '| Font size | Actor height |     | Font size | Actor size |'
+print '----------------------------     --------------------------'
+
+for size in range(1, 101):
+    label.set_font_name('Sans %s' %(size))
+    pt_height = label.get_height()
+    label.set_font_name('Sans %spx' %(size))
+    px_height = label.get_height()
+    print '|   %3dpt   |     %3dpx    |  %.2f   |   %3dpx   |    %3dpx   | %.2f' \
+                                            %(size, pt_height, pt_height/size, size, px_height, px_height/size)
+
+print '----------------------------     --------------------------'
