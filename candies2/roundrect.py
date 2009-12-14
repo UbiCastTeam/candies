b852ec43f1562a4c1d1af716ae765f447009e707
Damien Boucard <damien.boucard@ubicast.eu>
2009-12-14 16:23:17 +0100

    Adding OutlinedRoundRectangle actor which does not have background ;

--- candies2/roundrect.py
+++ candies2/roundrect.py
@@ -4,14 +4,8 @@ import clutter
 
 from clutter import cogl
 
-class RoundRectangle(clutter.Actor):
-    """
-    RoundRectangle (clutter.Actor)
-
-    A simple actor drawing a rectangle with round angles using the clutter.cogl
-    primitives.
-    """
-    __gtype_name__ = 'RoundRectangle'
+class OutlinedRoundRectangle(clutter.Actor):
+    __gtype_name__ = 'OutlinedRoundRectangle'
     __gproperties__ = {
         'color' : (
             str, 'color', 'Color', None, gobject.PARAM_READWRITE
@@ -20,6 +14,70 @@ class RoundRectangle(clutter.Actor):
             gobject.TYPE_FLOAT, 'Radius', 'Radius of the round angles',
             0.0, sys.maxint, 0.0, gobject.PARAM_READWRITE
         ),
+    }
+    
+    def __init__(self):
+        clutter.Actor.__init__(self)
+        self._color = clutter.color_from_string('Black')
+        self._radius = 0.0
+    
+    def set_color(self, color):
+        self._color = clutter.color_from_string(color)
+        self.queue_redraw()
+    
+    def set_radius(self, radius):
+        self._radius = radius
+        self.queue_redraw()
+    
+    def do_set_property(self, pspec, value):
+        if pspec.name == 'color':
+            self._color = clutter.color_from_string(value)
+        elif pspec.name == 'radius':
+            self._radius = value
+        else:
+            raise TypeError('Unknown property ' + pspec.name)
+        self.queue_redraw()
+
+    def do_get_property(self, pspec):
+        if pspec.name == 'color':
+            return self._color
+        elif pspec.name == 'radius':
+            return self._radius
+        else:
+            raise TypeError('Unknown property ' + pspec.name)
+    
+    def __paint_rectangle(self, width, height, color):
+        cogl.path_round_rectangle(0, 0, width, height, self._radius, 1)
+        cogl.path_close()
+        cogl.set_source_color(color)
+        cogl.path_stroke()
+    
+    def do_paint(self):
+        (x1, y1, x2, y2) = self.get_allocation_box()
+
+        paint_color = self._color
+        real_alpha = self.get_paint_opacity() * paint_color.alpha / 255
+        paint_color.alpha = real_alpha
+        
+        self.__paint_rectangle(x2 - x1, y2 - y1, paint_color)
+
+    def do_pick(self, pick_color):
+        if self.should_pick_paint() == False:
+            return
+
+        (x1, y1, x2, y2) = self.get_allocation_box()
+        self.__paint_rectangle(x2 - x1, y2 - y1, pick_color)
+
+
+class RoundRectangle(OutlinedRoundRectangle):
+    """
+    RoundRectangle (clutter.Actor)
+
+    A simple actor drawing a rectangle with round angles using the clutter.cogl
+    primitives.
+    """
+    __gtype_name__ = 'RoundRectangle'
+    __gproperties__ = {
         'border_color': (
             str, 'border color', 'Border color', None, gobject.PARAM_READWRITE
         ),
@@ -30,16 +88,10 @@ class RoundRectangle(clutter.Actor):
     }
 
     def __init__(self):
-        clutter.Actor.__init__(self)
-        self._color = clutter.color_from_string('Black')
+        OutlinedRoundRectangle.__init__(self)
         self._border_color = clutter.color_from_string('Black')
         self._border_width = 0.0
-        self._radius = 0.0
 
-    def set_color(self, color):
-        self._color = clutter.color_from_string(color)
-        self.queue_redraw()
-    
     def set_border_color(self, color):
         self._border_color = clutter.color_from_string(color)
         self.queue_redraw()
@@ -48,34 +100,23 @@ class RoundRectangle(clutter.Actor):
         self._border_width = width
         self.queue_redraw()
 
-    def set_radius(self, radius):
-        self._radius = radius
-        self.queue_redraw()
-
     def do_set_property(self, pspec, value):
-        if pspec.name == 'color':
-            self._color = clutter.color_from_string(value)
-        elif pspec.name == 'border-color':
+        if pspec.name == 'border-color':
             self._border_color = clutter.color_from_string(value)
         elif pspec.name == 'border-width':
             self._border_width = value
-        elif pspec.name == 'radius':
-            self._radius = value
         else:
-            raise TypeError('Unknown property ' + pspec.name)
+            OutlinedRoundRectangle.do_set_property(self, pspec, value)
+            return
         self.queue_redraw()
 
     def do_get_property(self, pspec):
-        if pspec.name == 'color':
-            return self._color
-        elif pspec.name == 'border-color':
+        if pspec.name == 'border-color':
             return self._border_color
         elif pspec.name == 'border-width':
             return self._border_width
-        elif pspec.name == 'radius':
-            return self._radius
         else:
-            raise TypeError('Unknown property ' + pspec.name)
+            return OutlinedRoundRectangle.do_get_property(self, pspec)
 
     def __paint_rectangle(self, width, height, color, border_color=None):
         cogl.path_round_rectangle(0, 0, width, height, self._radius, 1)
@@ -92,7 +133,7 @@ class RoundRectangle(clutter.Actor):
 
         cogl.set_source_color(color)
         cogl.path_fill()
-
+    
     def do_paint(self):
         (x1, y1, x2, y2) = self.get_allocation_box()
 
@@ -106,27 +147,27 @@ class RoundRectangle(clutter.Actor):
 
         self.__paint_rectangle(x2 - x1, y2 - y1, paint_color, border_color)
 
-    def do_pick(self, pick_color):
-        if self.should_pick_paint() == False:
-            return
-
-        (x1, y1, x2, y2) = self.get_allocation_box()
-        self.__paint_rectangle(x2 - x1, y2 - y1, pick_color)
-
-gobject.type_register(RoundRectangle)
 
 if __name__ == '__main__':
     stage = clutter.Stage()
     stage.set_size(640, 480)
     stage.connect('destroy', clutter.main_quit)
 
+    rect = OutlinedRoundRectangle()
+    rect.set_radius(25)
+    rect.set_color('Red')
+    rect.set_size(160, 120)
+    rect.set_anchor_point(80, 60)
+    rect.set_position(160, 240)
+    stage.add(rect)
+    
     rect = RoundRectangle()
     rect.set_radius(25)
     rect.set_color('Blue')
     rect.set_border_width(5)
-    rect.set_size(320, 240)
-    rect.set_anchor_point(160, 120)
-    rect.set_position(320, 240)
+    rect.set_size(160, 120)
+    rect.set_anchor_point(80, 60)
+    rect.set_position(480, 240)
     stage.add(rect)
 
     stage.show()
