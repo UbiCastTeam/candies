568a86668b22818c020dba8687565a7516a21e68
lavi <lavi@annie>
2011-12-14 17:30:55 +0100

    _stroke with has same size

--- candies2/stattracer.py
+++ candies2/stattracer.py
@@ -4,6 +4,7 @@
 import gobject
 import clutter
 from clutter import cogl
+import math
 
 class Tracer(clutter.Actor):
     """
@@ -15,12 +16,13 @@ class Tracer(clutter.Actor):
         'color' : (str, 'color', 'Color', None, gobject.PARAM_READWRITE),
     }
 
-    def __init__(self, color='Green', n=25, stroke_width=3):
+    def __init__(self, color='Green', n=50, stroke_width=3, with_scale=False):
         clutter.Actor.__init__(self)
         self._stroke_width = stroke_width
         self._color = clutter.color_from_string(color)
         self.percent = list()
         self.n = n
+        self.with_scale = with_scale
 
     def update(self, value):
         self.percent.append(value)
@@ -48,48 +50,76 @@ class Tracer(clutter.Actor):
             raise TypeError('Unknown property ' + pspec.name)
 
     def __cogl_path(self, width, height, color):
-        cogl.set_source_color(clutter.color_from_string('#ffffffff'))
-
-        #rect
-        cogl.path_move_to(0,0)
-        cogl.path_line_to(0,height)
-        cogl.path_line_to(width,height)
-        cogl.path_line_to(width,0)
-        cogl.path_line_to(0,0)
-        cogl.path_line_to(3,3)
-        cogl.path_line_to(3,height-3)
-        cogl.path_line_to(width-3,height-3)
-        cogl.path_line_to(width-3,3)
-        cogl.path_line_to(3,3)
-        cogl.path_close()
-        cogl.path_fill()
-        #0.25 stroke
-        cogl.path_rectangle(0,height*0.25,width, height*0.25+1)
-        cogl.path_fill()
-        cogl.path_close()
-        #0.5 stroke
-        cogl.path_rectangle(0,height*0.5,width,height*0.5+1)
-        cogl.path_close()
-        cogl.path_fill()
-        #0.75 stroke
-        cogl.path_rectangle(0,height*0.75, width, height*0.75+1)
-        cogl.path_close()
-        cogl.path_fill()
+        
+        if self.with_scale :
+            cogl.set_source_color(clutter.color_from_string('#ffffffff'))
+            #rect
+            cogl.path_move_to(0,0)
+            cogl.path_line_to(0,height)
+            cogl.path_line_to(width,height)
+            cogl.path_line_to(width,0)
+            cogl.path_line_to(0,0)
+            cogl.path_line_to(3,3)
+            cogl.path_line_to(3,height-3)
+            cogl.path_line_to(width-3,height-3)
+            cogl.path_line_to(width-3,3)
+            cogl.path_line_to(3,3)
+            cogl.path_close()
+            cogl.path_fill()
+            #0.25 stroke
+            cogl.path_rectangle(0,height*0.25,width, height*0.25+1)
+            cogl.path_fill()
+            cogl.path_close()
+            #0.5 stroke
+            cogl.path_rectangle(0,height*0.5,width,height*0.5+1)
+            cogl.path_close()
+            cogl.path_fill()
+            #0.75 stroke
+            cogl.path_rectangle(0,height*0.75, width, height*0.75+1)
+            cogl.path_close()
+            cogl.path_fill()
 
         #stats
         cogl.set_source_color(color)
+        scale_x = width/float(self.n)
+        
         for i, value in enumerate(self.percent):
             y = height - (value*height)/100
-            if y == 0 : y= self._stroke_width
-            x = i*width/self.n
+            x = i*scale_x
             if i == 0:
                 cogl.path_move_to(x,y)
             else :
                 cogl.path_line_to(x,y)
 
         for i, value in enumerate(reversed(self.percent)):
-            y = abs(height - (value*height)/100 - self._stroke_width)
-            x = (len(self.percent)-i -1)*width/self.n
+            x = (len(self.percent)-i -1)*scale_x
+            y = height - (value*height)/100
+            if i != 0 and i != len(self.percent) -1  :
+                diff_y = (value - self.percent[-i])*height/100
+                a = diff_y/scale_x
+                if a == 0 :
+                    dist = self._stroke_width
+                else :
+                    alpha = math.atan(1/a)
+                    dist = self._stroke_width/math.sin(alpha)
+                b = y - a*x + abs(dist)
+                
+                diff_y = (self.percent[-i-2] - value)*height/100
+                c = diff_y/scale_x
+                if c == 0 :
+                    dist = self._stroke_width
+                else :
+                    alpha = math.atan(1/c)
+                    dist = self._stroke_width/math.sin(alpha)
+                d = y - c*x + abs(dist)
+                
+                if a-c != 0.0  :
+                    x = (d-b)/(a-c)
+                    y = a*x + b
+                else :
+                    y = y + self._stroke_width
+            else :
+                y = y + self._stroke_width
             cogl.path_line_to(x,y)
         cogl.path_close()
         cogl.path_fill()
@@ -114,12 +144,13 @@ if __name__ == '__main__':
     stage.set_size(640, 480)
     stage.connect('destroy', clutter.main_quit)
 
-    percent = [1,10,20,100,50,40,30,40,50]
+    percent = [1,10,20,100,50,40,45,40,50,42]
+    percent = [1,50,100,40,1]
 
-    test = Tracer()
-    #test.percent = percent
+    test = Tracer(n=len(percent)-1)
+    test.percent = percent
     test.set_color('Red')
-    test.set_size(500, 50)
+    test.set_size(400, 100)
     test.set_position(10, 240)
     stage.add(test)
 
