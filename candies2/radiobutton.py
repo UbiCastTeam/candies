86ca1c5cf40d65191d612ccad7fadb84ecb46ed1
Sebastien de Melo <sebastien.de-melo@ubicast.eu>
2012-05-25 11:12:46 +0200

    remove bugged line

--- candies2/radiobutton.py
+++ candies2/radiobutton.py
@@ -56,7 +56,6 @@ class RadioBox(CheckBox):
     def _on_press(self, source, event):
         if not self.checked:
             self.toggle_check()
-            self.on_press()
             for btn in self._related_buttons:
                 btn.set_checked(False)
     
