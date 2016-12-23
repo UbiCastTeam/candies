b56d52dba2016b4b0bf512c4aaa65d948d8502d8
Stéphane Diemer <stephane.diemer@ubicast.eu>
2016-12-23 15:08:49 +0100

    Handle border in disk.

--- candies2/disk.py
+++ candies2/disk.py
@@ -15,10 +15,11 @@ class Disk(Clutter.Actor):
     A simple actor drawing a disk using cairo.
     '''
 
-    def __init__(self, color=None, stroke_width=50):
+    def __init__(self, color=None, border_color=None, border_width=5):
         super(Disk, self).__init__()
-        self.color = get_rgb_color(color or 'black')
-        self.stroke_width = stroke_width
+        self.color = get_rgb_color(color) if color else None
+        self.border_color = get_rgb_color(border_color) if border_color else None
+        self.border_width = border_width
 
         self.canvas = Clutter.Canvas()
         self.set_content(self.canvas)
@@ -26,10 +27,13 @@ class Disk(Clutter.Actor):
         self.connect('notify::allocation', self.on_allocation)
 
     def set_color(self, color):
-        self.color = get_rgb_color(color)
+        self.color = get_rgb_color(color) if color else None
 
-    def set_stroke_width(self, width):
-        self.stroke_width = width
+    def set_border_color(self, color):
+        self.border_color = get_rgb_color(color) if color else None
+
+    def set_border_width(self, width):
+        self.border_width = width
 
     def on_allocation(self, *kwargs):
         GObject.idle_add(self.idle_resize)
@@ -41,32 +45,47 @@ class Disk(Clutter.Actor):
         # reset canvas
         ctx.set_operator(cairo.OPERATOR_CLEAR)
         ctx.paint()
+        if not self.color and not self.border_color:
+            return
         ctx.set_operator(cairo.OPERATOR_OVER)
 
-        if width == height:
-            radius = width / 2.
-            ctx.arc(radius, radius, radius, 0, 2 * math.pi)
+        x = self.border_width
+        y = self.border_width
+        w = width - self.border_width * 2
+        h = height - self.border_width * 2
+
+        if w == h:
+            radius = w / 2.
+            ctx.arc(x + radius, y + radius, radius, 0, 2 * math.pi)
         else:
-            x_radius = width / 2.
-            y_radius = height / 2.
+            x_radius = w / 2.
+            y_radius = h / 2.
             ctx.save()
-            ctx.translate(x_radius, y_radius)
+            ctx.translate(x + x_radius, y + y_radius)
             ctx.scale(x_radius, y_radius)
             ctx.arc(0, 0, 1, 0, 2 * math.pi)
             ctx.restore()
         ctx.close_path()
-        ctx.set_source_rgb(*self.color)
-        ctx.fill()
+        if self.color:
+            ctx.set_source_rgb(*self.color)
+            ctx.fill_preserve()  # fill but keep the rectangle
+        if self.border_color:
+            ctx.set_line_width(self.border_width)
+            ctx.set_source_rgb(*self.border_color)
+            ctx.stroke()
 
 
 def tester(stage):
-    actor = Disk()
-    actor.set_color('red')
-    actor.set_size(200, 200)
-    actor.set_anchor_point(100, 100)
-    actor.set_position(320, 240)
-    stage.add_child(actor)
-    GObject.timeout_add_seconds(2, actor.set_size, 100, 400)
+    d1 = Disk(color='blue')
+    d1.set_size(200, 200)
+    d1.set_position(50, 50)
+    stage.add_child(d1)
+    GObject.timeout_add_seconds(2, d1.set_size, 100, 400)
+
+    d2 = Disk(border_color='red')
+    d2.set_size(200, 200)
+    d2.set_position(300, 50)
+    stage.add_child(d2)
 
 
 if __name__ == '__main__':
